9f94cf0653530c292fdba61a94540ffd44da6902
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-02 18:17:53 +0100

    Create user model

---
+++ models/user.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, Unique } from 'typeorm';
+import BaseModel from './base';
+
+@Entity({ name: 'users' })
+@Unique(['email'])
+export default class User extends BaseModel {
+	@Column('text', { nullable: false })
+	firstname!: string;
+
+	@Column('text', { nullable: false })
+	lastname!: string;
+
+	@Column('text', { nullable: false })
+	email!: string;
+
+	@Column('text', { nullable: false, select: false })
+	password!: string;
+
+	@Column('text', { nullable: false })
+	location!: string;
+
+	@Column('text', { nullable: false, array: true })
+	favouriteCategories!: string[];
+}
