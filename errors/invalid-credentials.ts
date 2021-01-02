0eb3a47d1426dcec76e69998629300b03a4736bf
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-02 18:20:41 +0100

    Create special errors for authentication module

---
+++ errors/invalid-credentials.ts
@@ -0,0 +1,7 @@
+import BaseHttpError from './base-http';
+
+export default class InvalidCredentialsError extends BaseHttpError {
+	constructor() {
+		super(401, 'Invalid username and/or password');
+	}
+}
