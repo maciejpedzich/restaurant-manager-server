88c12ebb51eee10cea67c1a40eb5c35b1936609e
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-09 19:02:10 +0100

    Create product model

---
+++ interfaces/order-product.ts
@@ -0,0 +1,4 @@
+export default interface OrderProduct {
+	name: string;
+	quantity: number;
+}
---
+++ models/product.ts
@@ -0,0 +1,28 @@
+import { Entity, Column } from 'typeorm';
+import BaseEntity from '../models/base';
+
+export enum Availability {
+	IN_STOCK = 'In stock',
+	LOW_STOCK = 'Low stock',
+	OUT_OF_STOCK = 'Out of stock'
+}
+
+@Entity({ name: 'products' })
+export default class Product extends BaseEntity {
+	@Column('text', { nullable: false })
+	name!: string;
+
+	@Column('decimal', { nullable: false, precision: 5, scale: 2 })
+	price!: number;
+
+	@Column('text', { nullable: false })
+	imageUrl!: string;
+
+	//prettier-ignore
+	@Column({
+		type: 'enum',
+		enum: Availability,
+		default: Availability.IN_STOCK
+	})
+	availability!: Availability;
+}
