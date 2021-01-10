b06be4a249471bb288c5f45afac8f4e2cf4e62ce
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-10 16:45:13 +0100

    Use validationMiddleware on register and logIn

--- routers/auth.ts
+++ routers/auth.ts
@@ -3,13 +3,24 @@ import { Router } from 'express';
 import AuthService from '../services/auth';
 import authMiddleware from '../middleware/auth';
 import generateTokensMiddleware from '../middleware/generate-tokens';
+import validationMiddleware from '../middleware/validation';
+import User from '../models/user';
 
 const authService = new AuthService();
 const authRouter = Router();
 
-authRouter.post('/register', authService.register);
+// prettier-ignore
+authRouter.post(
+  '/register',
+  validationMiddleware(User),
+  authService.register
+);
 
-authRouter.post('/login', authService.logIn);
+authRouter.post(
+	'/login',
+	validationMiddleware(User, { skipMissingProperties: true }),
+	authService.logIn
+);
 
 authRouter.post('/refresh', authMiddleware);
 
