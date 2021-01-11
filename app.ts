f394bcf7aaf0fbb2f96cd0bc7ff0adc367932b23
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-11 15:51:59 +0100

    Use productsRouter and uploadImageRouter

--- app.ts
+++ app.ts
@@ -5,8 +5,14 @@ import { config as loadENV } from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-import errorMiddleware from './middleware/error';
+import uploadImageRouter from './routers/upload-image';
 import authRouter from './routers/auth';
+import productsRouter from './routers/products';
+
+import authMiddleware from './middleware/auth';
+import hasPermissionsMiddleware from './middleware/has-permissions';
+import fileUploadMiddleware from './middleware/file-upload';
+import errorMiddleware from './middleware/error';
 
 loadENV();
 
@@ -31,10 +37,13 @@ const isDevelopmentEnv = process.env.NODE_ENV === 'development';
 				exposedHeaders: ['Authorization']
 			})
 		);
+		app.use(express.static('public'));
 		app.use(cookieParser());
 		app.use(express.json());
 
-		app.use('/auth', authRouter);
+		app.use('/upload-image', uploadImageRouter);
+		app.use('/api/auth', authRouter);
+		app.use('/api/products', productsRouter);
 		app.use(errorMiddleware);
 
 		app.listen(process.env.PORT);
