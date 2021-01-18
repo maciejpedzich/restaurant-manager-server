ad6f7b6de376c5219a732fa4b0878761bb074378
Maciej Pędzich <dnalop.me@gmail.com>
2021-01-18 16:21:31 +0100

    Change EmailRegisteredError message

--- errors/email-registered.ts
+++ errors/email-registered.ts
@@ -2,6 +2,6 @@ import BaseHttpError from './base-http';
 
 export default class EmailRegisteredError extends BaseHttpError {
   constructor() {
-    super(409, 'This email address is already registered');
+    super(409, 'This email address has already been registered');
   }
 }
